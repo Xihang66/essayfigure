1334d4602ea0da7bd83af175d73605e40dbbf5e8
Xihang Yuan <100748608+Xihang66@users.noreply.github.com>
2022-03-05 10:52:10 +0800

    Update radiation.m

--- radiation.m
+++ radiation.m
@@ -1,4 +1,4 @@
 %function I=radiation(h,c0,Kb,Tsolar)
 syms lamda;
 h=6.62606896*10^-34;c0=3*10^8;Kb=1.3806488*10^-23;Tsolar=5800;
-I=2*h*c0*c0/lamda^5/(exp(h*c0/lambda/Kb/Tsolar)-1);
+I=2*h*c0*c0/lamda^5/(exp(h*c0/lamda/Kb/Tsolar)-1);
